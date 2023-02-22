e2e030654a206260cb6bd06f2b8da2d714a8500f
Micah Iserman <micah.iserman@gmail.com>
2023-02-22 11:24:14 -0500

    changes source of ACS county estimates

--- docs/functions/api.js
+++ docs/functions/api.js
@@ -4,6 +4,8 @@ const DataHandler = require('../data_handler.v1.min.js'),
     block_group: require('../block_group.json'),
     tract: require('../tract.json'),
     county: require('../county.json'),
+    health_district: require('../health_district.json'),
+    school_district: require('../school_district.json'),
   })
 module.exports.handler = async function (event) {
   return data.export(event.queryStringParameters)
